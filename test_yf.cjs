f6a7af22991a3a00d61d96507692670ee869a473
SSQ <sonosgg@gmail.com>
2026-04-17 15:02:51 +0900

    chore: remove test files

--- test_yf.cjs
+++
@@ -1,7 +0,0 @@
-const yahooFinance = require('yahoo-finance2').default;
-async function test() {
-  const quote = await yahooFinance.quote('005930.KS');
-  console.log('shortName:', quote.shortName);
-  console.log('longName:', quote.longName);
-}
-test();
--- test_yf.mjs
+++
@@ -1,8 +0,0 @@
-import yahooFinance from 'yahoo-finance2';
-async function test() {
-  const quote = await yahooFinance.quote('005930.KS');
-  console.log('shortName:', quote.shortName);
-  console.log('longName:', quote.longName);
-  console.log('Keys:', Object.keys(quote));
-}
-test();
